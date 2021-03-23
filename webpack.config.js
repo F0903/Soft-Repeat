0dafa4fc55c2af493f76322797d152ceb2c18f86
Frederik P. B. H <frepbh@gmail.com>
2021-03-23 04:58:24 +0100

    updated folder naming

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./out_tsc/program.js",
+	entry: "./inter/out_tsc/program.js",
 	output: {
 		filename: "main.js",
-		path: path.resolve("./out/"),
+		path: path.resolve("./dist/js/"),
 	},
 };
