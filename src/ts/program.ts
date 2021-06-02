73dd78b5e328381a86e77e82befb0b929bf00b40
Frederik P. B. H <frepbh@gmail.com>
2021-06-02 03:03:47 +0200

    version bump

--- src/ts/program.ts
+++ src/ts/program.ts
@@ -1,9 +1,6 @@
 import Repeater from "./repeater/repeater";
 import { OnElementExistsWithClass } from "./utility/dom";
 
-//TODO: Make a larger refactor
-//"menu-container"
-
 OnElementExistsWithClass(
 	"title style-scope ytd-video-primary-info-renderer",
 	async (x) => new Repeater().Start(x)
