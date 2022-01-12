ac12e90d15708d91d7967feada05f90fcee0a926
Frederik P <frepbh@gmail.com>
2022-01-12 16:42:53 +0100

    Added todo's

--- src/ts/program.ts
+++ src/ts/program.ts
@@ -3,6 +3,9 @@ import KeyBlocker from "./utility/keyblocker";
 import { OnAttributeChanged } from "./utility/observer";
 import { OnElementExists } from "./utility/dom";
 
+//TODO: Fix check exiting when scrubbing.
+//TODO: Fix key blocking only working when first opening menu, then clicking off again.
+
 let repeater: Repeater; // To be sure it wont be GC'd
 
 function ManageKeyBlocker(menu: HTMLElement) {
